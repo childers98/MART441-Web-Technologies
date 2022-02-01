2db09da733cfa9d7d8a9772948d23a88738246c6
childers98 <70028131+childers98@users.noreply.github.com>
2022-01-31 20:39:13 -0700

    Week 2 - Console.log Elements

---
+++ Week-2/js/script.js
@@ -0,0 +1,23 @@
+//favorite websites
+console.log('Redbubble'+' '+'https://www.redbubble.com/explore/for-you/');
+console.log('Maalavidaa'+ ' ' + 'https://www.maalavidaa.com/');
+console.log('Cool Math Games - Rotate and Roll'+ ' ' + 'https://www.coolmathgames.com/0-rotate-and-roll');
+
+//Favorite Games
+console.log('Favorite Games')
+console.log('Red Dead Redemption' + ', ' + 'The Last of Us Part II' + ', ' + 'and' + ' ' + 'Uncharted');
+
+//Favorite Artists
+console.log('Favorite Artists');
+console.log('Ludacris');
+console.log('Helped foster a new light in the rap industry by creating more light hearted and fun songs.');
+
+console.log('Maya Lin');
+console.log('Maya Lin usually does sculptures of natural things.  She entered and won a contest to make a veteran memorial.  A lot of people did not want the memorial to be black but it drove home the reflectiveness to be had with honoring veterans.');
+
+console.log('Rittz');
+console.log('A smaller rapper bringing attention to the fact that many people still think rap is something only black people should do.  He has made the effort to understand the industry so that he can be involved in it.');
+/*Comment
+out
+mulitple
+lines*/
---
+++ Week-2/readme.md
@@ -0,0 +1,3 @@
+This week went pretty well I think the Codeacademy was great.  I also downloaded grasshopper to practice with when I'm away from my computer.  Learning the console.log element wasn't too bad because I had a friend who had showed it to me during previous classes.
+
+I wasn't exactly sure how to make things display on the page using JavaScript but I did fiddle with that a little bit and looked at some other people's code.  Did not understand quite what they were trying to do with their stuff.
